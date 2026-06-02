449cfd747201053917007ff7be5c7d2c643e8f7c
Tristan Prudencio <sebastianprudencio964@gmail.com>
2026-06-01 20:35:24 -0600

    practica 2 terminada final

--- app/main.py
+++ app/main.py
@@ -4,18 +4,20 @@ from app.infrastructure.database import models
 from app.adapters.http.producto_router import producto_router
 from app.adapters.http.perfil_compra_router import perfil_compra_router
 from app.adapters.http.orden_compra_router import orden_compra_router
+from app.adapters.http.auth_router import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="PuzzleStore API",
-    description="E-commerce de cubos Rubik, rompecabezas y puzzles — Arquitectura Hexagonal",
+    description="E-commerce de cubos Rubik, rompecabezas y puzzles",
     version="1.0.0"
 )
 
 app.include_router(producto_router, tags=["Productos"])
 app.include_router(perfil_compra_router, tags=["Perfiles de Compra"])
 app.include_router(orden_compra_router, tags=["Órdenes de Compra"])
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
